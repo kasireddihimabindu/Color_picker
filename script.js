9f444e32a4a69812825c3afbd6f03fe53b8599a8
kasireddihimabindu <kasireddi.himabindu@gmail.com>
2024-07-28 20:03:23 +0530

    Initial references

---
+++ script.js
@@ -0,0 +1,9 @@
+let pickColor = document.getElementsByClassName("pick");
+let error = document.getElementById("error");
+let fileInput = document.getElementsById("file");
+let image = document.getElementById("image");
+let hexValRef = document.getElementById("hex-valref");
+let rgbValRef = document.getElementById("rgb-valref");
+let customAlert = document.getElementById("custom-alert");
+let pickedColorRef = document.getElementById("picked-color-ref");
+let eyeDropper;
